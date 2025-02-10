5f476bf329336873764ce6aa2d1c35fc89cccc98
Eeva Rontti <t2roee00@students.oamk.fi>
2025-02-10 14:44:34 +0200

    .

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+
 
 app.get('/api/frontpage', (req, res) => {
 res.json({ message: 'This is frontpage' });
 });
 
-app.listen(port, () => {
-console.log('Server is running at http://localhost:${port}');
+app.listen(3000, () => {
+console.log('Server is running at http://localhost:3000');
 });
